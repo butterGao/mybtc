c752975490dd426b45c42e42dfcae88cb093da57
butterGao <494132814@qq.com>
2017-01-19 16:13:13 +0800

    重写日志,为以后迁移日志埋点

---
+++ src/main/java/com/clevo/regular/Regular.java
@@ -0,0 +1,13 @@
+package com.clevo.regular;
+
+import java.math.BigDecimal;
+
+/**
+ * ^_^
+ * Created by gaojiawei on 2017/1/16.
+ */
+public interface Regular {
+	public BigDecimal getBuyPrice ();
+
+	public BigDecimal getSellPrice();
+}
---
+++ src/main/java/com/clevo/trade/TradeService.java
@@ -0,0 +1,12 @@
+package com.clevo.trade;
+
+/**
+ * ^_^
+ * Created by gaojiawei on 2017/1/16.
+ * 交易服务
+ */
+public interface TradeService {
+	public void start();
+
+	public void loginfo ();
+}
