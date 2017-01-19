307b871de039789f3fbc2ab5e90ae7ed369a9f69
butterGao <494132814@qq.com>
2017-01-19 14:45:40 +0800

    重写日志,为以后迁移日志埋点

---
+++ src/main/java/com/clevo/trade/TradeTask.java
@@ -0,0 +1,17 @@
+package com.clevo.trade;
+
+
+import java.util.TimerTask;
+
+/**
+ * ^_^
+ * Created by gaojiawei on 2017/1/16.
+ */
+public class TradeTask extends TimerTask {
+
+
+	@Override
+	public void run () {
+
+	}
+}
